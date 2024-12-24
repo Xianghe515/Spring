4da2f818513715c05acdb370e6920789b7510366
Admin <tkdgur8999@gmail.com>
2024-12-24 13:08:36 +0900

    12.24

--- src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -1,38 +1,72 @@
 package com.thehecklers.sburrestdemo;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
 @SpringBootApplication
+@ConfigurationPropertiesScan
 public class SburRestDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SburRestDemoApplication.class, args);
     }
+
+    // Droid 객체 빈 등록
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    public Droid droid() {
+        return new Droid();
+    }
+
+
 }
 
+// 초기 데이터를 생성하는 컴포넌트 구현
+@Component
+class DataLoader {
+    private final CoffeeRepository coffeeRepository;
+    public DataLoader(CoffeeRepository coffeeRepository) {
+        this.coffeeRepository = coffeeRepository;
+    }
+
+    @PostConstruct
+    private void loadData() {
+        this.coffeeRepository.saveAll(List.of(
+            new Coffee("Cafe Cereze"),
+            new Coffee("Cafe Ganador"),
+            new Coffee("Cafe Lareno"),
+            new Coffee("Cafe Tres Pontas")
+        ));
+    }
+
+}
     @RestController
     @RequestMapping("/coffees")
     class RestApiController {
+    // ch4 3. 저장소 사용하기
         // 처리할 데이터
-        private List<Coffee> coffees = new ArrayList<>();
+//        private List<Coffee> coffees = new ArrayList<>();
+// 저장소 호출(spring의 생성자 주입)
+        private final CoffeeRepository coffeeRepository;
 
-        // 생성자
-        public RestApiController() {
-            coffees.addAll(List.of(
-                new Coffee("Cafe Cereze"),
-                new Coffee("Cafe Ganador"),
-                new Coffee("Cafe Lareno"),
-                new Coffee("Cafe Tres Pontas")
-            ));
+        // 생성자(수정)
+        public RestApiController(CoffeeRepository coffeeRepository) {
+            this.coffeeRepository = coffeeRepository;
         }
 
         /*
@@ -43,48 +77,46 @@ public class SburRestDemoApplication {
 //        @RequestMapping(value = "/coffees", method = RequestMethod.GET)
         @GetMapping
         Iterable<Coffee> getCoffees() {
-            return coffees;
+            return coffeeRepository.findAll();
         }
         // 특정 ID를 통해 불러오기
         @GetMapping("/{id}")
         Optional<Coffee> getCoffee(@PathVariable String id) {
-            for (Coffee coffee : coffees) {
-                if (coffee.getId().equals(id)) {
-                    return Optional.of(coffee);
-                }
-            }
-            return Optional.empty();
+            return coffeeRepository.findById(id);
         }
         // POST - 생성
         @PostMapping
         Coffee postCoffee(@RequestBody Coffee coffee) {
-            coffees.add(coffee);
-            return coffee;
+            return coffeeRepository.save(coffee);
         }
         // PUT
         @PutMapping("/{id}")
         ResponseEntity<Coffee> putCoffee(@PathVariable String id, @RequestBody Coffee coffee) {
-            int coffeeIndex = -1;
+            // 1. id를 이용해 객체가 있는지 확인
+            // 2. 있으면 전달 받은 coffee 객체를 이용해 수정
+            // 3. 없으면 전달 받은 coffee 객체를 이용해 새롭게 생성
 
-            for (Coffee c : coffees) {
-                if (c.getId().equals(id)) {
-                    coffeeIndex = coffees.indexOf(c);
-                    coffees.set(coffeeIndex, coffee);
-                }
-            }
-            return (coffeeIndex == -1) ? new ResponseEntity<>(postCoffee(coffee),HttpStatus.CREATED) : new ResponseEntity<>(coffee,HttpStatus.OK);
+            return (coffeeRepository.existsById(id))
+                    ? new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.OK)
+                    : new ResponseEntity<>(coffeeRepository.save(coffee), HttpStatus.CREATED);
         }
         // DELETE
         @DeleteMapping("/{id}")
-        void deleteCoffee(@PathVariable String id) {
-            coffees.removeIf(c -> c.getId().equals(id));
+        String deleteCoffee(@PathVariable String id) {
+            coffeeRepository.deleteById(id);
+            return coffeeRepository.existsById(id) ? "삭제 실패" : "삭제 성공";
         }
     }
+// coffee 엔티티에 대한 저장소(repository)
+interface CoffeeRepository extends CrudRepository<Coffee, String> {
 
+}
 
     // 도메인 생성(coffee)
+@Entity(name = "coffes")
     class Coffee {
         // 필드
+        @Id
         private String id;
         private String name;
 
